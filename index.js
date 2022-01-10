3d03d2766f6bd766c401789894c8492d59a8742c
Amith <amithharish019@gmail.com>
2022-01-11 01:09:35 +0530

    Initial commit

--- index.js
+++ index.js
@@ -9,41 +9,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     btnAnimation(btnpress);
   });
 }
-//
-//     switch(btnInHTML)
-//     {
-//       case "w":
-//       var waudio=new Audio("sounds/crash.mp3");
-//       waudio.play();
-//       break;
-//       case "a":
-//       var aaudio=new Audio("sounds/kick-bass.mp3");
-//       aaudio.play();
-//       break;
-//       case "s":
-//       var saudio=new Audio("sounds/snare.mp3");
-//       saudio.play();
-//       break;
-//       case "d":
-//       var daudio=new Audio("sounds/tom-1.mp3");
-//       daudio.play();
-//       break;
-//       case "j":
-//       var jaudio=new Audio("sounds/tom-2.mp3");
-//       jaudio.play();
-//       break;
-//       case "k":
-//       var kaudio=new Audio("sounds/tom-3.mp3");
-//       kaudio.play();
-//       break;
-//       case "l":
-//       var laudio=new Audio("sounds/tom-4.mp3");
-//       laudio.play();
-//       break;
-//     }
-//
-//   });
-// }
+
 
 document.addEventListener("keypress",function(event)
 {
